cb371dda8ad1a12a35ad1dd111bbee987107452b
Rivadeneiea <joserivadeneira538@gmail.com>
2023-08-23 21:23:09 -0300

    creamos puerto

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
-console.log("esto es una prueba 2");
 // 1 configuraciones iniciales
-
+const app = express();
+// crear variablle con express
+app.set("port", process.env.PORT || 4000);
 // middlewares (funciones)
 
 // crear las rutas
